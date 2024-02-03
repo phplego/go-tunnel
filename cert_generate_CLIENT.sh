282bf669266df59460488d4b55019f14fefbc4e5
dub <phplego@gmail.com>
2024-02-04 03:52:02 +0700

    passin/passout cert gen for client

--- cert_generate_CLIENT.sh
+++ cert_generate_CLIENT.sh
@@ -6,14 +6,14 @@ NAME=client
 
 openssl req -x509 -newkey rsa:4096 \
     -CA $CA.crt -CAkey $CA.key \
-    -keyout $NAME.key -out $NAME.crt -days 3650 -passout pass:1234 \
+    -keyout $NAME.key -out $NAME.crt -days 3650 -passin pass:1234 -passout pass:1234 \
     -addext "basicConstraints=CA:FALSE"
 
 echo "export to the normal format (for browsers etc).."
-openssl pkcs12 -export -out $NAME.p12 -inkey $NAME.key -in $NAME.crt -passin pass:1234
+openssl pkcs12 -export -out $NAME.p12 -inkey $NAME.key -in $NAME.crt -passin pass:1234 -passout pass:1234
 
 echo "export to the legacy format (for android etc).."
-openssl pkcs12 -export -out $NAME.legacy.p12 -inkey $NAME.key -in $NAME.crt -passin pass:1234
+openssl pkcs12 -export -out $NAME.legacy.p12 -inkey $NAME.key -in $NAME.crt -passin pass:1234 -passout pass:1234
 
 
 
